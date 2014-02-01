adc2ed1954678619bff5a1f9f7fdc8348ef5bc47
Rob Beagrie <rob@beagrie.com>
2014-02-01 19:45:49 +0000

    Changed url definition to reflect new app structure

--- django_resume/urls.py
+++ django_resume/urls.py
@@ -2,6 +2,6 @@
 from django.conf.urls import patterns, url
 
 
-urlpatterns = patterns('django_resume.resume.views',
+urlpatterns = patterns('django_resume.views',
     url(r'^$', 'index', name='resume_home'),
 )
