423c02a6c418063adab78fa890987883c40c8e6f
Rob Beagrie <rob@beagrie.com>
2014-02-01 19:27:30 +0000

    Add setup.py for installation

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='django_resume',
+      version='0.1',
+      description='Django Resume is a Django app built to generate a resume on a user's site.',
+      author='Chris Kelly, Rob Beagrie',
+      author_email='rob@beagrie.com',
+      url='https://github.com/rbeagrie/django-resume',
+      packages=['django-resume'],
+     )
