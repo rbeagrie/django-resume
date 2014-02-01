f49f6ac6361476d608c42dedd0b44d418d93a229
Rob Beagrie <rob@beagrie.com>
2014-02-01 19:25:19 +0000

    Changed folder structure to make a reusable app

--- django_resume/urls.py
+++ django_resume/urls.py
@@ -1,20 +1,7 @@
-from django.conf.urls import patterns, include, url
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.conf import settings
+# Resume App URLs file
+from django.conf.urls.defaults import *
 
-# Uncomment the next two lines to enable the admin:
-from django.contrib import admin
-admin.autodiscover()
 
-urlpatterns = patterns('',
-    (r'^$', include('django_resume.resume.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+urlpatterns = patterns('django_resume.resume.views',
+    url(r'^$', 'index', name='resume_home'),
 )
-
-if settings.DEBUG:
-    urlpatterns += staticfiles_urlpatterns()
