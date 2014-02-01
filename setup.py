8bfa6057053ff3d20523882e0247503d44ecba45
Rob Beagrie <rob@beagrie.com>
2014-02-01 19:31:46 +0000

    Fixed typos in setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from distutils.core import setup
 
 setup(name='django_resume',
       version='0.1',
-      description='Django Resume is a Django app built to generate a resume on a user's site.',
+      description='Django Resume is a Django app built to generate a resume on a user\'s site.',
       author='Chris Kelly, Rob Beagrie',
       author_email='rob@beagrie.com',
       url='https://github.com/rbeagrie/django-resume',
-      packages=['django-resume'],
+      packages=['django_resume'],
      )
